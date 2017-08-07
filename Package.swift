2e9ec25a9959a24e4584b7e9756868213cc66f79
Advaita Pandya <advaita.pandya@rakuten.com>
2017-08-07 13:42:34 +0900

    Added missing Package.swift

---
+++ Package.swift
@@ -0,0 +1,10 @@
+// swift-tools-version:3.1
+
+import PackageDescription
+
+let package = Package(
+    name: "YouTubeFloatingPlayer",
+    dependencies: [
+    	.Package(url: "https://github.com/youtube/youtube-ios-player-helper.git")
+    ]
+)
